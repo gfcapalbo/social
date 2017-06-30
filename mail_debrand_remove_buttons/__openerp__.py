c5f5b22600083746733116e6fdff24216c032d0a
Giovanni Francesco Capalbo <giovanni@therp.nl>
2017-06-30 12:18:05 +0200

    9.0 add module mail_debrand_and_remove_buttons

---
+++ mail_debrand_remove_buttons/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Tecnativa - Jairo Llopis
+# Copyright 2017 SerpentCS - Darshan Patel
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Mail Debrand and remove buttons",
+    "summary": "Remove Odoo branding and buttons in sent emails",
+    "version": "9.0.1.0.0",
+    "category": "Social Network",
+    "website": "https://www.tecnativa.com",
+    "author": "Tecnativa, "
+              "Eficent, "
+              "Therp B.V., "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "data": ["data/mail_data.xml"],
+    "depends": [
+        "mail",
+    ],
+}
