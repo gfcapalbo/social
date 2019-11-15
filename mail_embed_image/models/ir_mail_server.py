9c7c8f30b874abe0aa08bfe731ec89ca59fabbfc
Giovanni Francesco Capalbo <giovanni@therp.nl>
2019-11-15 16:42:51 +0100

    [REF] refactor

--- mail_embed_image/models/ir_mail_server.py
+++ mail_embed_image/models/ir_mail_server.py
@@ -3,6 +3,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 import uuid
 import logging
+import threading
+from contextlib import contextmanager
 from odoo import models, http
 from odoo.addons.base.ir.ir_mail_server import encode_header_param
 from werkzeug.wrappers import Request
@@ -12,7 +14,6 @@ from lxml.etree import tostring
 from base64 import encodestring
 from email.mime.image import MIMEImage
 from odoo.http import root as root_wsgi
-import threading
 
 
 logger = logging.getLogger(__name__)
@@ -20,6 +21,19 @@ logger = logging.getLogger(__name__)
 
 class IrMailServer(models.Model):
     _inherit = 'ir.mail_server'
+
+    @contextmanager
+    def as_user(self, env, user):
+        # the current user is public user
+        olduser = env.user
+        old_self_user = self.env.user
+        env.user = user
+        self.env.user = user
+        try:
+            yield
+        finally:
+            env.user = olduser
+            self.env.user = old_self_user
 
     def build_email(
             self,
@@ -61,64 +75,72 @@ class IrMailServer(models.Model):
         """ Given a message, find it's img tags and if they
         are URLs, replace them with cids.
         """
-        base_url = self.env['ir.config_parameter'].get_param(
-            'web.base.url')
         for part in email.walk():
             if part.get_content_maintype() == 'text':
                 body = part.get_payload(decode=True)
                 if not body or body == '\n':
                     continue
                 root = fromstring(body)
-                for img in root.xpath(
-                        "//img[starts-with(@src, '%s/web/image')]"
-                        "|"
-                        "//img[starts-with(@src, '/web/image')]" % (
-                            base_url or '', )):
-                    # check if there is a bound request
-                    imgpath = img.get('src').replace(base_url, '')
-                    env = EnvironBuilder(imgpath).get_environ()
-                    endpoint, arguments = http.routing_map(
-                        self.env.registry._init_modules,
-                        False,
-                        self.env['ir.http']._get_converters(),
-                    ).bind_to_environ(
-                        env,
-                    ).match(return_rule=False)
-                    try:
-                        req = http.request.pop()
-                    except RuntimeError:
-                        # if there is not one, just create a fake one
-                        req = Request(env)
-                        # setup session
-                        session_store = root_wsgi.session_store
-                        session = session_store.new()
-                        session.update({
-                            'db': threading.current_thread().dbname,
-                            'login': self.env.user.login,
-                            'uid': self.env.user.id,
-                            'content': self.env.context,
-                            })
-                        req.session = session
-                        req = http.HttpRequest(req)
-                        http._request_stack.push(req)
-                    # now go ahead and call the endpoint and fetch the data
-                    response = endpoint.method(**arguments)
-                    if not response:
-                        logger.warning('Could not get %s', img.get('src'))
-                        continue
-                    cid = uuid.uuid4().hex
-                    filename_rfc2047 = encode_header_param(cid)
-                    filepart = MIMEImage(response.data)
-                    filepart.set_param('name', filename_rfc2047)
-                    filepart.add_header(
-                        'Content-Disposition',
-                        'inline',
-                        cid=cid,
-                        filename=filename_rfc2047,
-                    )
-                    # attach the image into the email as attachment
-                    email.attach(filepart)
-                    img.set('src', 'cid:%s' % (cid))
-                # encodebytes will put a newline every 74 char
+                self.process_img_body(root, email)
+                # encodestring will put a newline every 74 char
                 part.set_payload(encodestring(tostring(root)))
         return email
+
+    def process_img_body(self, root, email):
+        base_url = self.env['ir.config_parameter'].get_param(
+            'web.base.url')
+        for img in root.xpath(
+                "//img[starts-with(@src, '%s/web/image')]"
+                "|"
+                "//img[starts-with(@src, '/web/image')]" % (
+                    base_url or '', )):
+            # check if there is a bound request
+            imgpath = img.get('src').replace(base_url, '')
+            endpoint, arguments = self.fetch_image(imgpath)
+            # now go ahead and call the endpoint and fetch the data
+            response = endpoint.method(**arguments)
+            if not response:
+                logger.warning('Could not get %s', img.get('src'))
+                continue
+            cid = uuid.uuid4().hex
+            filename_rfc2047 = encode_header_param(cid)
+            filepart = MIMEImage(response.data)
+            filepart.set_param('name', filename_rfc2047)
+            filepart.add_header(
+                'Content-Disposition',
+                'inline',
+                cid=cid,
+                filename=filename_rfc2047,
+            )
+            # attach the image into the email as attachment
+            email.attach(filepart)
+            img.set('src', 'cid:%s' % (cid))
+
+    def fetch_image(self, image_path):
+        env = EnvironBuilder(image_path).get_environ()
+        with self.as_user(env, self.env.ref('base.public_user')):
+            endpoint, arguments = http.routing_map(
+                self.env.registry._init_modules,
+                False,
+                self.env['ir.http']._get_converters(),
+            ).bind_to_environ(
+                env,
+            ).match(return_rule=False)
+            try:
+                req = http.request.pop()
+            except RuntimeError:
+                # if there is not one, just create a fake one
+                req = Request(env)
+                # setup session
+                session_store = root_wsgi.session_store
+                session = session_store.new()
+                session.update({
+                    'db': threading.current_thread().dbname,
+                    'login': self.env.user.login,
+                    'uid': self.env.user.id,
+                    'content': self.env.context,
+                    })
+                req.session = session
+                req = http.HttpRequest(req)
+                http._request_stack.push(req)
+            return endpoint, arguments
