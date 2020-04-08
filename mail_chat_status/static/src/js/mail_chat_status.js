f4e36f83a1a975751db800849f652b2a8b3bec79
Giovanni Francesco Capalbo <giovanni@therp.nl>
2020-04-08 15:19:33 +0200

    f

--- mail_chat_status/static/src/js/mail_chat_status.js
+++ mail_chat_status/static/src/js/mail_chat_status.js
@@ -2,10 +2,10 @@ odoo.define('mail_chat_status.chat_manager', function (require) {
     "use strict";
     var chat_manager = require('mail.chat_manager');
 
-    chat_manager.include({
-        make_channel: function (data, options) {
+chat_manager.include({
+     function make_channel (data, options) {
             debugger;
-            channel = this._super.apply((data,options);
+            channel = this._super(data, options);
             if (channel_type == "dm") {
                 channel.direct_partner = data.direct_partner[0]
                 channel.status_description = channel.direct_partner.status_description
